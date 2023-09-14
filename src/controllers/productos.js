c5db4bc7bf6f4845271513d170c401cacd25fc90
Seiseishon <francoocapo7@gmail.comgit config --global user.email francoocapo7@gmail.com>
2023-09-14 02:26:37 -0300

    Agregue el listado de productos y cambie algo del controlador create

--- src/controllers/productos.js
+++ src/controllers/productos.js
@@ -1,34 +1,50 @@
 const { isUtf8 } = require('buffer');
 const { json } = require('express');
-const fs = require ('fs');
+const fs = require('fs');
 const path = require('path');
 
+
+const dato = ()=>{
+    const rawDatos = fs.readFileSync(path.resolve(__dirname, '../datos/datos.json'), 'utf-8')
+   
+   return JSON.parse(rawDatos);
+}
+
 const controladorProducts = {
-    producto: function(req,res){
+    productCreate: function (req, res) {
         res.render('create')
     },
-    detail: function(req,res){
-        res.render('productDetail')},
-    create:function(req,res){
-            const newProduct={
-                id:new Date().getTime(),
-                name:req.body.name,
-                price:req.body.price,
-                img:req.body.image,
-                envio:req.body.envio,
-                
-            }
-            const products=JSON.parse(fs.readFileSync(path.resolve(__dirname,'../datos/datos.json'),'Utf8'))
-            products.push(newProduct)
-           
-            const data=JSON.stringify(products)
-            console.log(data)
-            fs.writeFileSync(path.resolve(__dirname,'../datos/datos.json'),data)
-            res.redirect('/')
+    detail: function (req, res) {
+        res.render('productDetail')
+    },
+    create: function (req, res) {
+        const newProduct = {
+            id: new Date().getTime(),
+            name: req.body.name,
+            price: req.body.price,
+            img: req.body.image,
+            ver: "ver mas...",
+            carrito: "carrito.jpg",
+            envio:"$" + req.body.envio,
+
         }
+        const products = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../datos/datos.json'), 'Utf8'))
+        products.push(newProduct)
+
+        const data = JSON.stringify(products)
+        console.log(data)
+        fs.writeFileSync(path.resolve(__dirname, '../datos/datos.json'), data)
+        res.redirect('/')
+    },
+    producto: function (req,res){
+        const products = dato()
+        res.render('productos',{
+            products
+        })
+    }
 
     
 }
 
 
-    module.exports = controladorProducts
+module.exports = controladorProducts
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -3,8 +3,9 @@ const controladorProducts = require('../controllers/productos');
 
 const routerProduct = express.Router();
 
-routerProduct.get('/create', controladorProducts.producto)
+routerProduct.get('/create', controladorProducts.productCreate)
 routerProduct.get('/productDetail', controladorProducts.detail)
+routerProduct.get('/',controladorProducts.producto)
 
 routerProduct.post('/create',controladorProducts.create)
 module.exports = routerProduct;
